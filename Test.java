ae1ece1d915b8850b76e70bc67a52a455cfde01e
Flynn Antonacci <flynn.antonacci@live.com>
2021-11-27 22:42:36 -0500

    initial final commit

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+  public static void main(String[] args){
+    System.out.println("My name is Flynn");
+    System.out.println("I am 23 years old");
+    System.out.println("My hometown is North Augusta, SC");
+  }
+}
+
+//'javac' is not recognized as an internal or external command, operable program or batch file.
+// also jdk 8 doesn't configure to vscode I need version 11 and I downloaded that but vscode doesn't recognize that either. 
+//What terminal was the lady using in the lesson video?
